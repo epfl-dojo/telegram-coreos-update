e49576ee527b136615baae5dd88e4e2a3e45dfc9
ponsfrilus <ponsfrilus@gmail.com>
2017-04-07 18:47:12 +0200

    Using telegraf (instead of node-telegram-bot-api)
    * telegraf allows to use the parse_mode `markdown`
    * layout of the `/latest` msg updated

--- index.js
+++ index.js
@@ -1,44 +1,61 @@
-var TelegramBot = require('node-telegram-bot-api');
-var fs = require("fs"),
-    secrets = JSON.parse(fs.readFileSync('./secrets.json')),
-    request = require("request"),
-    semver = require("semver");
+const Telegraf = require('telegraf')
+var
+  fs = require("fs"),
+  secrets = JSON.parse(fs.readFileSync('./secrets.json')),
+  request = require("request"),
+  semver = require("semver")
 
-// replace the value below with the Telegram token you receive from @BotFather
-var token = secrets.token;
+const app = new Telegraf(secrets.token || process.env.BOT_TOKEN)
 
-// Create a bot that uses 'polling' to fetch new updates
-var bot = new TelegramBot(token, {
-    polling: true
-});
+app.command('start', (ctx) => {
+  console.log('start', ctx.from)
+  ctx.reply(`_Hey counter:_ s`, {parse_mode: 'Markdown'})
+  ctx.reply('Welcome!')
+})
 
-bot.onText(/\/latest/, function(msg, match) {
-    var coreosRelease = request('https://coreos.com/releases/releases.json', function(error, response, body) {
-        console.log('error:', error); // Print the error if one occurred
-        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+/*
+https://core.telegram.org/bots/api#markdown-style
+*bold text*
+_italic text_
+[text](http://www.example.com/)
+`inline fixed-width code`
+```text
+pre-formatted fixed-width code block
+```
+*/
+app.command('latest', (ctx) => {
+  request('https://coreos.com/releases/releases.json', function(error, response, body) {
+    // console.log('error:', error); // Print the error if one occurred
+    // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
 
-        var parsed = JSON.parse(body);
+    var parsed = JSON.parse(body)
+    var sorted_versions = Object.keys(parsed).sort( (a, b) => {
+        return semver.compare(b, a)
+      });
+    var latest_v = sorted_versions[0]
+    var latest_info = parsed[sorted_versions[0]]
+    // https://coreos.com/releases/#1298.7.0
+    var latest_url = 'https://coreos.com/releases/#' + latest_v
+    // https://github.com/coreos/manifest/releases/tag/v1298.7.0
+    var latest_gh_url = 'https://github.com/coreos/manifest/releases/tag/v' + latest_v
+    var release_message = '*CoreOS latest release:* \n'
+    release_message += ' ⇨ ['+ latest_v +']('+ latest_url +') ‣ [GitHub]('+ latest_gh_url +') link\n'
+    release_message += ' \- date: ' + latest_info.release_date + '\n'
+    release_message += ' \- kernel: ' + latest_info.major_software.kernel + '\n'
+    release_message += ' \- docker: ' + latest_info.major_software.docker + '\n'
+    release_message += ' \- etcd: ' + latest_info.major_software.etcd + '\n'
+    release_message += ' \- fleet: ' + latest_info.major_software.fleet + '\n'
+    release_message += '\n*Release notes:*\n'
 
-        var sorted_versions = Object.keys(parsed).sort(
-            function(a, b) {
-                return semver.compare(b, a);
-            }
-        );
-        bot.sendMessage(msg.chat.id, JSON.stringify(parsed[sorted_versions[0]]));
-    });
-    bot.sendMessage(msg.chat.id, "Hang on, Imma look it up");
-});
+    release_message += latest_info.release_notes
 
-// Matches "/echo [whatever]"
-bot.onText(/\/echo (.+)/, function(msg, match) {
-    // 'msg' is the received Message from Telegram
-    // 'match' is the result of executing the regexp above on the text content
-    // of the message
+    ctx.reply(release_message, {parse_mode: 'Markdown'})
+  });
+  ctx.reply(`_...Hang on, Imma look it up..._`, {parse_mode: 'Markdown'})
+})
 
-    console.log("received /echo");
-    var chatId = msg.chat.id;
-    var resp = match[1]; // the captured "whatever"
+app.hears('hi', (ctx) => ctx.reply('Hey there!'))
 
-    // send back the matched "whatever" to the chat
-    bot.sendMessage(chatId, resp);
-});
+app.on('sticker', (ctx) => ctx.reply('👍'))
+
+app.startPolling()
