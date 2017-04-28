25f2f507a3e6d9c085f67d9e74f63db47d783f73
ponsfrilus <ponsfrilus@gmail.com>
2017-04-28 15:56:59 +0200

    Refactoring
    * Bot "stable", "beta", "alpha" sub commands refactored with an array
    * `/latest stable`, `/latest beta` and `/latest alpha` commands are
      now working as expected
    * `/start` command is smarter

--- index.js
+++ index.js
@@ -16,19 +16,22 @@ The Telegram API is wrapped with telegraf <https://github.com/telegraf/telegraf/
       ```text
       pre-formatted fixed-width code block
       ```
-
 */
 
 const Telegraf = require('telegraf')
 const Extra = Telegraf.Extra
 const Markup = Telegraf.Markup
+const ReleaseChannels = ["stable", "beta", "alpha"]
+const commandParts = require('telegraf-command-parts');
+
 
 var
   fs = require("fs"),
   request = require("request"),
   semver = require("semver"),
   pp = require("properties-parser"),
-  token = ''
+  token = '',
+  releaseAnswers = {}
 
 if (fs.existsSync('./secrets.json')) {
   // In case you want to add a secrets.json file with
@@ -50,6 +53,7 @@ if (token == '') {
   process.exit(1);
 }
 const app = new Telegraf(token)
+app.use(commandParts());
 
 /* Generic function to generate msg with release version info */
 function releaseNotes(parsed, version, channel) {
@@ -58,75 +62,63 @@ function releaseNotes(parsed, version, channel) {
   var latest_url = 'https://coreos.com/releases/#' + version
   // https://github.com/coreos/manifest/releases/tag/v1298.7.0
   var latest_gh_url = 'https://github.com/coreos/manifest/releases/tag/v' + version
-  var release_message = '*CoreOS latest '+ channel +' release:* \n'
-      release_message += ' ⇨ ['+ version +']('+ latest_url +') ‣ [GitHub]('+ latest_gh_url +') link\n'
-      release_message += ' \- date: ' + latest_info.release_date + '\n'
-      release_message += ' \- kernel: ' + latest_info.major_software.kernel + '\n'
-      release_message += ' \- docker: ' + latest_info.major_software.docker + '\n'
-      release_message += ' \- etcd: ' + latest_info.major_software.etcd + '\n'
-      release_message += ' \- fleet: ' + latest_info.major_software.fleet + '\n'
-      release_message += '\n*Release notes:*\n'
-      release_message += latest_info.release_notes
+  var release_message = '*CoreOS latest ' + channel + ' release:* \n'
+  release_message += ' ⇨ [' + version + '](' + latest_url + ') ‣ [GitHub](' + latest_gh_url + ') link\n'
+  release_message += ' \- date: ' + latest_info.release_date + '\n'
+  release_message += ' \- kernel: ' + latest_info.major_software.kernel + '\n'
+  release_message += ' \- docker: ' + latest_info.major_software.docker + '\n'
+  release_message += ' \- etcd: ' + latest_info.major_software.etcd + '\n'
+  release_message += ' \- fleet: ' + latest_info.major_software.fleet + '\n'
+  release_message += '\n*Release notes:*\n'
+  release_message += latest_info.release_notes
   return release_message
 }
 
-app.command('start', (ctx) => {
+app.command('latest alpha', (ctx) => {
   console.log('start', ctx.from)
-  ctx.reply(`_Hey counter:_ s`, {parse_mode: 'Markdown'})
-  ctx.reply('Welcome!')
+  ctx.reply('Welcome! Please use the /latest command...')
 })
 
 /* LATEST : show a quick menu offering `stable`, `beta` and `alpha` */
 app.command('latest', (ctx) => {
-  return ctx.reply('One time keyboard', Markup
-    .keyboard([
-      '/stable',
-      '/alpha',
-      '/beta'
-    ])
-    .oneTime()
-    .resize()
-    .extra()
-  )
+  if (ctx.state.command.splitArgs[0] != '') {
+    var release = ctx.state.command.splitArgs[0]
+    // @todo check standard releases
+    return releaseAnswers[release](ctx)
+  } else {
+    return ctx.reply('One time keyboard', Markup
+      .keyboard(ReleaseChannels.map(function(releaseChannel) {
+        return '/' + releaseChannel
+      }))
+      .oneTime()
+      .resize()
+      .extra()
+    )
+  }
 })
 
-/* STABLE */
-app.command('stable', (ctx) => {
-  request('https://stable.release.core-os.net/amd64-usr/current/version.txt', function(error, response, body) {
-    var latest_stable_version = pp.parse(body).COREOS_VERSION_ID
-    request('https://coreos.com/releases/releases.json', function(error, response, body) {
-      var parsed = JSON.parse(body)
-      release_message = releaseNotes(parsed, latest_stable_version, 'stable')
-      ctx.reply(release_message, {parse_mode: 'Markdown'})
+/**
+ * @param releaseChannel Either "stable", "beta" or "alpha"
+ */
+function defineCommand(releaseChannel) {
+  var releaseAnswer = function(ctx) {
+    request('https://' + releaseChannel + '.release.core-os.net/amd64-usr/current/version.txt', function(error, response, body) {
+      var latest_version = pp.parse(body).COREOS_VERSION_ID
+      request('https://coreos.com/releases/releases.json', function(error, response, body) {
+        var parsed = JSON.parse(body)
+        release_message = releaseNotes(parsed, latest_version, releaseChannel)
+        ctx.reply(release_message, {
+          parse_mode: 'Markdown'
+        })
+      });
     });
-  });
-  ctx.reply(`_...Hang on, Imma look up the latest stable..._`, {parse_mode: 'Markdown'})
-})
+    ctx.reply('_...Hang on, Imma look up the latest ' + releaseChannel + '..._', {
+      parse_mode: 'Markdown'
+    })
+  }
+  releaseAnswers[releaseChannel] = releaseAnswer
+  app.command(releaseChannel, releaseAnswer)
+}
 
-/* BETA */
-app.command('beta', (ctx) => {
-  request('https://beta.release.core-os.net/amd64-usr/current/version.txt', function(error, response, body) {
-    var latest_beta_version = pp.parse(body).COREOS_VERSION_ID
-    request('https://coreos.com/releases/releases.json', function(error, response, body) {
-      var parsed = JSON.parse(body)
-      release_message = releaseNotes(parsed, latest_beta_version, 'ϐeta')
-      ctx.reply(release_message, {parse_mode: 'Markdown'})
-    });
-  });
-  ctx.reply(`_...Hang on, Imma look up the latest beta..._`, {parse_mode: 'Markdown'})
-})
-
-/* ALPHA */
-app.command('alpha', (ctx) => {
-  request('https://alpha.release.core-os.net/amd64-usr/current/version.txt', function(error, response, body) {
-    var latest_alpha_version = pp.parse(body).COREOS_VERSION_ID
-    request('https://coreos.com/releases/releases.json', function(error, response, body) {
-      var parsed = JSON.parse(body)
-      release_message = releaseNotes(parsed, latest_alpha_version, 'αlpha')
-      ctx.reply(release_message, {parse_mode: 'Markdown'})
-    });
-  });
-  ctx.reply(`_...Hang on, Imma look up the latest alpha..._`, {parse_mode: 'Markdown'})
-})
-
+ReleaseChannels.map(defineCommand);
 app.startPolling()
